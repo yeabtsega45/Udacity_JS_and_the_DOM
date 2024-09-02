e19928dde9736390d7dd44e1a3c92b82dbbb8082
yeabtsega45 <yabtsega2022@gmail.com>
2024-09-02 21:52:14 +0300

    Create content with JavaScript

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-// Update Program Title
+// 9.7 Update Program Title
 const card__content = document.querySelector(".card__content--h5");
 card__content.innerHTML = "<em>Everything</em> You Need to Know About Data";
 
-// Add Page Content
+// 9.15 Add Page Content
 const page__content = document.querySelector(".testimonials");
 const newH2 = document.createElement("h2");
 const newH2Text = document.createTextNode("Advice for New Udacity Students");
@@ -14,3 +14,16 @@ const newP =
 			for your coursework. Add those blocks to your calendar and \
 			set reminders.</p>";
 newH2.insertAdjacentHTML("afterend", newP);
+
+// 9.27 Style Page Content
+const start__now = document.querySelector("#start-now");
+start__now.style.cssText = "background-color: #2015ff; border-radius: 5rem;";
+
+const hero__homepage = document.querySelector(".hero--homepage");
+const newHero = hero__homepage.classList.add("new-hero");
+hero__homepage.appendChild(newHero);
+
+const cardList = document.querySelectorAll(".card");
+for (i = 0; i < cardList.length; i++) {
+  cardList[i].classList.add("new-card");
+}
